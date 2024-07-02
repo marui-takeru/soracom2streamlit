063899f814b637c826a89d983439a8deb95de2fb
丸井 健（愛媛大学） <149051458+marui-takeru@users.noreply.github.com>
2024-07-02 17:51:18 +0900

    Update devapp.py

--- devapp.py
+++ devapp.py
@@ -214,7 +214,7 @@ if response.status_code == 200:
         st.pyplot(fig)
 
         # 表の作成
-        st.write(df.set_index('日付').drop(columns=['傾斜角Z', '傾斜角X（縦方向）', '傾斜角Y（横方向）']))
+        st.write(df.set_index('日付').drop(columns=['傾斜角Z', '傾斜角X（縦方向）', '傾斜角Y（横方向）', 'Predicted_X']))
             
     else:
         st.error('過去7日分のデータが存在しません。')
