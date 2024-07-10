c21b0e4ed01ed335807d5b351656f17458ba81f5
丸井 健（愛媛大学） <149051458+marui-takeru@users.noreply.github.com>
2024-07-10 19:55:04 +0900

    Update devapp2.py

--- devapp2.py
+++ devapp2.py
@@ -5,6 +5,7 @@ import requests
 import datetime
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
 
 # APIの認証情報を環境変数から取得
 api_username = st.secrets.APIs.api_username
@@ -24,7 +25,7 @@ url09 = st.secrets.APIs.url09
 url10 = st.secrets.APIs.url10
 
 # Streamlit app
-st.title('【開発版2】堂野窪 傾斜センサ')
+st.title('詳細　堂野窪 傾斜センサ')
 
 # APIキーとトークンを作成
 auth = (api_username, api_password)
@@ -78,28 +79,48 @@ url_display_names = {
     "１０：ヒラノジ": url10
 }
 
-# Fetch data for all URLs
-all_data = []
+# Select a URL using a dropdown
+selected_display_name = st.selectbox('閲覧したい傾斜センサを選んでください', list(url_display_names.keys()))
+selected_url = url_display_names[selected_display_name]
 
-for display_name, url in url_display_names.items():
-    response = requests.get(url, headers=headers, params=params)
+# Fetch data for the selected URL
+response = requests.get(selected_url, headers=headers, params=params)
 
-    if response.status_code == 200:
-        data = response.json()
-        for item in data:
-            inclination = item['content'].split(',')
-            inclination.append(display_name)
-            all_data.append(inclination)
-    else:
-        st.error(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+if response.status_code == 200:
+    data = response.json()
 
-# Create DataFrame
-if all_data:
-    df = pd.DataFrame(all_data, columns=['日付', '傾斜角X', '傾斜角Y', '傾斜角Z', '電圧', '気温', '湿度', 'センサー'])
+    # Create DataFrame
+    inclination = []
+    for item in data:
+        inclination.append(item['content'].split(','))
+
+    # 前回の値を保持するための変数
+    prev_value = None
+    
+    # 数値型に変換し、閾値条件を適用する関数
+    def convert_to_numeric_with_threshold(value):
+        global prev_value
+        try:
+            current_value = float(value)
+            if prev_value is None:
+                prev_value = current_value
+                return current_value
+    
+            # 前回の値との差が3度以上の場合はNaNを返す
+            if abs(current_value - prev_value) >= 3:
+                return float('NaN')
+    
+            # 差が3度未満の場合はそのまま数値に変換
+            prev_value = current_value
+            return current_value
+        except ValueError:
+            return float('NaN')
+
+    df = pd.DataFrame(inclination, columns=['日付', '傾斜角X', '傾斜角Y', '傾斜角Z', '電圧', '気温', '湿度'])
 
     # Convert columns to appropriate data types
     df['日付'] = pd.to_datetime(df['日付'], errors='coerce')
-    df['傾斜角X（縦方向）'] = df['傾斜角X'].apply(pd.to_numeric, errors='coerce')
+    df['傾斜角X（縦方向）'] = df['傾斜角X'].apply(convert_to_numeric_with_threshold)
     df['気温'] = pd.to_numeric(df['気温'], errors='coerce')
     
     # NaNを含む行を削除する
@@ -107,71 +128,86 @@ if all_data:
 
     # データが存在するかチェック
     if not df.empty:
-        # 各センサーごとにデータを処理
-        for sensor in url_display_names.keys():
-            st.subheader(sensor)
-            sensor_df = df[df['センサー'] == sensor]
+        # 最新のデータ取得時刻を表示
+        latest_date = df['日付'].max()
+        st.write(f'最新のデータ取得時刻：{latest_date}')
+        
+        # データ数の表示
+        num_samples = len(df)
+        
+        # 平均気温の計算
+        Tave = df['気温'].mean()
+    
+        # 選択した期間内のデータを使用して単回帰分析を行う
+        X = df['気温'].values.reshape(-1, 1)
+        y = df['傾斜角X（縦方向）'].values
+        
+        # 線形回帰モデルを構築
+        reg = LinearRegression().fit(X, y)
+        
+        # 回帰係数を取得
+        reg_coef = reg.coef_[0]
+    
+        # データの修正
+        df['補正角度'] = df['傾斜角X（縦方向）'] - reg_coef * (df['気温'] - Tave)
+    
+        # 前回の値との差分を計算して新しい列を追加
+        df['角度変化'] = -df['補正角度'].diff().shift(-1)
+        df['角度変化'].iloc[-1] = 0  # 最後の行に0を設定
+    
+        #  '角度変化'の最新値を取得
+        latest_diff_x = df['角度変化'].iloc[0]
+        st.write(f'最新の角度変化：{latest_diff_x}')
+    
+        # 背景色の設定
+        background_color = '#ffffff'  # Default white
+        if 0.0 <= abs(latest_diff_x) < 0.05:
+            background_color = '#ccffcc'  # Green
+        elif 0.05 <= abs(latest_diff_x) < 0.1:
+            background_color = '#ffff99'  # Yellow
+        else:
+            background_color = '#ff9999'  # Red
 
-            # データが存在するかチェック
-            if not sensor_df.empty:
-                # 平均気温の計算
-                Tave = sensor_df['気温'].mean()
+        background_color_css = f"""
+        <style>
+            .stApp {{
+                background-color: {background_color};
+            }}
+        </style>
+        """
+        st.markdown(background_color_css, unsafe_allow_html=True)
+        
+        # グラフのプロット
+        fig, ax = plt.subplots(figsize=(10, 5))  # 1x1のサブプロットを作成
+        
+        # '日付' を x 軸、'Diff_X' を y 軸にプロット
+        ax.plot(df['日付'], df['角度変化'], label='Sabun', color='black')
+        ax.set_title('Kakudo Henka')
+        ax.set_xlabel('YYYY-MM-DD')  # x 軸のラベルを設定
+        ax.set_ylabel('Kakudo Henka')  # y 軸のラベルを設定
+        ax.legend()
+    
+        # 縦軸のレンジを -0.2 から 0.2 までで固定
+        ax.set_ylim(-0.2, 0.2)
 
-                # 選択した期間内のデータを使用して単回帰分析を行う
-                X = sensor_df['気温'].values.reshape(-1, 1)
-                y = sensor_df['傾斜角X（縦方向）'].values
+        # 縦軸の値が0.05と0.1の場所に波線を描画
+        ax.axhline(y=0.05, color='black', linestyle='dotted', label='注意')
+        ax.axhline(y=-0.05, color='black', linestyle='dotted', label='注意')
+        ax.axhline(y=0.1, color='red', linestyle='dotted', label='危険')
+        ax.axhline(y=-0.1, color='red', linestyle='dotted', label='危険')
+        
+        
+        # Streamlit でグラフを表示
+        st.pyplot(fig)
 
-                # 線形回帰モデルを構築
-                reg = LinearRegression().fit(X, y)
-
-                # 回帰係数を取得
-                reg_coef = reg.coef_[0]
-
-                # データの修正
-                sensor_df['補正角度'] = sensor_df['傾斜角X（縦方向）'] - reg_coef * (sensor_df['気温'] - Tave)
-
-                # 前回の値との差分を計算して新しい列を追加
-                sensor_df['角度変化'] = -sensor_df['補正角度'].diff().shift(-1)
-                sensor_df['角度変化'].iloc[-1] = 0  # 最後の行に0を設定
-
-                # 最新のデータ取得時刻を表示
-                latest_date = sensor_df['日付'].max()
-
-                # '角度変化'の最新値を取得
-                latest_diff_x = sensor_df['角度変化'].iloc[0] if not sensor_df.empty else 'N/A'
-
-                # 背景色の設定
-                background_color = '#ffffff'  # Default white
-                if 0.0 <= abs(latest_diff_x) < 0.05:
-                    background_color = '#ccffcc'  # Green
-                elif 0.05 <= abs(latest_diff_x) < 0.1:
-                    background_color = '#ffff99'  # Yellow
-                else:
-                    background_color = '#ff9999'  # Red
-
-                # Display the latest values with background color
-                st.markdown(
-                    f"""
-                    <div style="background-color: {background_color}; padding: 10px; border-radius: 5px;">
-                        <p><strong>最新のデータ取得時刻：</strong>{latest_date}</p>
-                        <p><strong>最新の角度変化：</strong>{latest_diff_x}</p>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
-                # 表の作成
-                # st.write(sensor_df)
-            else:
-                st.warning(f'{sensor}のデータが存在しません。')            
+        # 表の作成
+        # 表示したい列の順番を指定
+        columns_order = ['角度変化', '補正角度', '傾斜角X', '電圧', '気温', '湿度']
+        
+        # 表を表示
+        st.write(df.set_index('日付')[columns_order])
+        
     else:
         st.error('データが存在しません。')
 else:
-    st.error('データが取得できませんでした。')
-
-# クリックすると各傾斜計デバイスを表示するアプリに移動
-st.markdown(
-    """
-    詳細は[こちら](https://soracom2app-devapp2.streamlit.app/)をクリックしてください。
-    """,
-    unsafe_allow_html=True
-)
+    st.error(f"Failed to fetch data from {selected_url}. Status code: {response.status_code}")
