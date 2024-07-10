20dda02aab65511b82dc5a6d17134edb33daa5e1
丸井 健（愛媛大学） <149051458+marui-takeru@users.noreply.github.com>
2024-07-10 19:25:16 +0900

    Update devapp2.py

--- devapp2.py
+++ devapp2.py
@@ -137,7 +137,10 @@ if all_data:
             st.write(f'最新のデータ取得時刻：{latest_date}')
             
             # '角度変化'の最新値を取得
+            latest_diff_x = sensor_df['角度変化'].iloc[0] if not sensor_df.empty else 'N/A'
             st.write(f'最新の角度変化：{latest_diff_x}')
+            
+            st.write(sensor_df)
             
     else:
         st.error('データが存在しません。')
